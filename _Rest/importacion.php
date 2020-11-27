9cf83b09f308c3134b9673a3f32a93f253aa55c0
Mario Villatoro <ing.mario.villatoro@gmail.com>
2020-11-27 15:30:22 -0600

    actualizacion se agrega los impuestos

--- _Rest/importacion.php
+++ _Rest/importacion.php
@@ -44,14 +44,10 @@ if($cliente['tipo_descuento'] == 0)
     $letra_descuento = 'P';
     $monto_descuento = $cliente['tipo_descuento_gral_importe'];
     $pctje_descuento = $cliente['descuento_general'];
-    //$monto_descuento = $cliente['descuento_general'];
-    //$pctje_descuento = ($cliente['descuento_general_monto'] / $monto_descuento);
 }else if($cliente['tipo_descuento'] == 2){
     $letra_descuento = 'i';
     $monto_descuento = $cliente['tipo_descuento_gral_importe'];
     $pctje_descuento = $cliente['descuento_general'];
-    //$monto_descuento = $cliente['descuento_general'];
-    //$pctje_descuento = ($cliente['descuento_general_monto'] / $monto_descuento);
 }
 
 
@@ -60,8 +56,7 @@ $transaction = ibase_trans( IBASE_DEFAULT, $conexion );
 $query_insert_cotizacion = "INSERT INTO DOCTOS_VE(DOCTO_VE_ID, TIPO_DOCTO, SUBTIPO_DOCTO, FOLIO,FECHA,HORA,CLAVE_CLIENTE,CLIENTE_ID,DIR_CLI_ID,DIR_CONSIG_ID,MONEDA_ID,TIPO_CAMBIO,TIPO_DSCTO,DSCTO_PCTJE,DSCTO_IMPORTE,ESTATUS,APLICADO,DESCRIPCION, IMPORTE_NETO,FLETES,OTROS_CARGOS,TOTAL_IMPUESTOS,TOTAL_RETENCIONES,TOTAL_ANTICIPOS,PESO_EMBARQUE,FORMA_EMITIDA, CONTABILIZADO, ACREDITAR_CXC, SISTEMA_ORIGEN,COND_PAGO_ID, PCTJE_DSCTO_PPAG,VENDEDOR_ID, PCTJE_COMIS, VIA_EMBARQUE_ID,IMPORTE_COBRO, USUARIO_CREADOR, ES_CFD,ENVIADO, FECHA_HORA_ENVIO, CFD_ENVIO_ESPECIAL, CFDI_CERTIFICADO,FECHA_HORA_CREACION,USUARIO_ULT_MODIF,FECHA_HORA_ULT_MODIF,CARGAR_SUN) 
 values(GEN_ID(ID_DOCTOS,1), 'C', 'N', '".$folio."', '".date('d.m.Y')."' , '".date('H:i')."', '".$cliente['clave_cliente']."', ".$cliente['cliente_id'].", 
 ".$direccion->DIR_CLI_ID.", ".$direccion->DIR_CLI_ID.", 1, 0, '".$letra_descuento."', ".$pctje_descuento.",".$monto_descuento.", 'P', 'S', '".$cliente['descripcion']."', ".$cliente['subtotal'].", 0, 0, ". $cliente['iva'].", 0, 0, 0, 'N', 'N', 'N', 'VE', 302, 0, 1017, 0, 1014, 0, 'SYSDBA', 'N', 'N', NULL, 'N', 'N', '".date('d.m.Y H:i')."', 'SYSDBA', '23.11.2020 12:34' ,'S')";
-//echo  json_encode(array("estatus"=> 1, "texto"=> "Error al importar la cotización", "folio"=> $folio));
-//echo "\n";
+
 $result_INSERT = ibase_query($conection->getConexion(), $query_insert_cotizacion) or die(ibase_errmsg());
 if($result_INSERT)
 {
@@ -71,11 +66,20 @@ if($result_INSERT)
     while ($row = ibase_fetch_object ($result, IBASE_TEXT)){
         $id = $row->DOCTO_VE_ID;
     }
+//print_r($cliente);
+    //Insert importe
+    $importe_bruto = $cliente['subtotal'] + $cliente['descuento'] + $cliente['tipo_descuento_gral_importe'];
+    $query_insert_importe = "INSERT INTO IMPUESTOS_DOCTOS_VE(DOCTO_VE_ID, IMPUESTO_ID, VENTA_NETA, VENTA_BRUTA,OTROS_IMPUESTOS,PCTJE_IMPUESTO, IMPORTE_IMPUESTO, UNIDADES_IMPUESTO,IMPORTE_UNITARIO_IMPUESTO) 
+    values($id, 1015, ".$cliente['subtotal'].", ".$importe_bruto.",0,16, ".$cliente['iva'].",0,0)";
+    
+    $result_importe = ibase_query($conection->getConexion(), $query_insert_importe) or die(ibase_errmsg());
+    //
 
     $datos =  $data['datos'];
     //print_r($datos);
     $numero = count($datos);
     $contador = 0;
+    //print_r($datos);
     foreach ($datos as $key => $value) {
         //echo $value['clave_articulo'];
         $nota = "";
@@ -104,11 +108,41 @@ if($result_INSERT)
     PCTJE_DSCTO_PROMO, PRECIO_TOTAL_NETO, PCTJE_COMIS, ROL".$cabecera_nota.", POSICION) 
     values(GEN_ID(ID_DOCTOS,1), ".$id.", '".$value['clave_articulo']."', ".$value['articulo_id'].", ".$value['total'].", 0, 0, 0, ".$value['precio_unitario'].", ".$value['descuento_porcentaje'].", 
     ".$value['descuento_monto'].", ".$value['descuento_porcentaje']." , ".$value['descuento_general'].", 0, 0, ".$importe_neto.", 0, 'N'".$nota.", ".($key+1).")";
+    
+    
+    
+    //
     $insert_articulos = ibase_query($conection->getConexion(), $query_insert_cotizacion_detalles) or die(ibase_errmsg());
         if($insert_articulos)
         {
-            $contador++;
+            //VA ABAJO
+            $query_id = "SELECT MAX(DOCTO_VE_DET_ID) as DOCTO_VE_DET_ID FROM DOCTOS_VE_DET WHERE DOCTO_VE_ID='".$id."'";
+            $result_id = ibase_query($conection->getConexion(), $query_id) or die(ibase_errmsg());
+            $id_det = 0;
+            while ($row = ibase_fetch_object ($result_id, IBASE_TEXT)){
+                $id_det = $row->DOCTO_VE_DET_ID;
+            }
+
+            //$importe_bruto = $cliente['subtotal'] + $cliente['descuento'] + $cliente['tipo_descuento_gral_importe'];
+            $importe_impuesto_bruto = $value['subtotal'] * 0.16;
+            $venta_neta             = $value['subtotal'] - $value['descuento_general'] - $value['descuento_monto'];
+            $importe_impuesto       = $venta_neta * 0.16;
+            $query_insert_importe_det = "INSERT INTO IMPUESTOS_DOCTOS_VE_DET(DOCTO_VE_DET_ID, IMPUESTO_ID, DOCTO_VE_ID, ID_INTERNO_TIPO_IMPTO, TIPO_CALC,IMPORTE_IMPUESTO_BRUTO, VENTA_NETA, VENTA_BRUTA, OTROS_IMPUESTOS, PCTJE_IMPUESTO,IMPORTE_IMPUESTO, UNIDADES_IMPUESTO, IMPORTE_UNITARIO_IMPUESTO) 
+            values($id_det, 1015, $id, 'V', 'P',".$importe_impuesto_bruto.", ".$venta_neta.", ".$value['subtotal'].",0, 16, ".$importe_impuesto.",0,0)";
+            
+            $insert_articulos_importe = ibase_query($conection->getConexion(), $query_insert_importe_det) or die(ibase_errmsg());
+            if($insert_articulos)
+            {
+                $contador++;
+            }else
+            {
+                echo  json_encode(array("estatus"=> 0, "texto"=> "Error al importar la cotización"));
+                ibase_rollback($transaction);	
+            }
+            //echo "\n\r";
+            
         }else{
+            echo  json_encode(array("estatus"=> 0, "texto"=> "Error al importar la cotización"));
             ibase_rollback($transaction);	
         }
     }
